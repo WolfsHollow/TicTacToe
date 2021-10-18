cfe2b3647c16116555def5b491f0b43409e5b709
Wulfes <wolvenhollow@gmail.com>
2021-10-17 21:20:14 -0500

    revamped project from codepen

--- main.js
+++ main.js
@@ -1,47 +1,103 @@
-const gameboardReset = () =>{
-    let gameboard = [];
-    for (i = 0; i < 9; i++){
-        gameboard.push(null);
-    }
-    return gameboard;
+page = document.getElementById('page');
+gridWrapper = document.getElementById('gridWrapper');
+let box = [];
+
+let gameGrid = ['','','','','','','','',''];
+let playerTurn = 'X';
+
+(function createGrid() {
+  for (i=0; i < 9; i++){
+    box[i] = document.createElement('div');
+    box[i].setAttribute('class', 'box'); 
+    box[i].value = i;
+    box[i].addEventListener('click', changeColor);    
+    gridWrapper.appendChild(box[i]);
+  }
+})()
+
+function changeColor(){
+  let value = this.value;
+  let color = this.style.backgroundColor;
+  
+  if (playerTurn == 'X'){
+      this.style.backgroundColor = 'green';
+      this.innerText = playerTurn;  
+  }
+  else if (playerTurn == 'O'){
+       this.style.backgroundColor = 'darkblue';
+       this.innerText = playerTurn;
+  }
+  
+  gameGrid[value] = this.innerText;
+  checkWin(gameGrid);
+  changePlayer();
 }
 
-function createBoard(){
-    const page = document.getElementById('page');
-    const gameboardWrapper = document.querySelector('#gridWrapper');
-    let box = [];
-    for (i = 0; i < 9; i++){
-        box[i] = document.createElement('div');
-        box[i].setAttribute('class', 'box');
-        box[i].value = i;
-        box[i].addEventListener('click', (e)=>{console.log(e.target.value)});
-        gameboardWrapper.appendChild(box[i]);
+function checkWin(gameGrid){
+  let winner = false;
+  if (gameGrid[0] != ''){
+      if ((gameGrid[0] == gameGrid[1] && gameGrid[0] == gameGrid[2]) ||
+        (gameGrid[0] == gameGrid[4] && gameGrid[0] == gameGrid[8]) ||
+        (gameGrid[0] == gameGrid[3] && gameGrid[0] == gameGrid[6]))   
+      {
+        winner = true;
+      }
+  }
+  
+  if (gameGrid[2] != ''){
+     if (gameGrid[2] == gameGrid[5] && gameGrid[2] == gameGrid[8]){
+          winner = true;
+        } 
+     else if (gameGrid[2] == gameGrid[4] && gameGrid[2] == gameGrid[6]){
+          winner = true;
+        }
+  }
+  
+  if (gameGrid[4] != ''){
+    if (gameGrid[1] == gameGrid[4] && gameGrid[1] == gameGrid[7]){
+        winner = true;
     }
-
-    box[1].setAttribute('class', 'border');
-    box[1].style.borderTop = 0;
-    box[1].style.borderBottom = 0;
-
-    box[3].setAttribute('class', 'border');
-    box[3].style.borderLeft = 0;
-    box[3].style.borderRight = 0;
-
-
-    box[4].setAttribute('class', 'border');
-    box[5].setAttribute('class', 'border');
-    box[5].style.borderLeft = 0;
-    box[5].style.borderRight = 0;
-
-    box[7].setAttribute('class', 'border');
-    box[7].style.borderTop = 0;
-    box[7].style.borderBottom = 0;
+     else if (gameGrid[3] == gameGrid[4] && gameGrid[3] == gameGrid[5]){
+        winner = true;
+    }
+  }  
+  if (gameGrid[6] != ''){
+      if (gameGrid[6] == gameGrid[7] && gameGrid[6] == gameGrid[8]){
+        winner = true;
+      }   
+  }  
+  
+  let spaceIndex = gameGrid.indexOf('');
+  if (winner==true){
+    alert(`${playerTurn} has won!`);
+    console
+  }
+  if (spaceIndex == -1 && winner != true){
+     return alertTie();
+   }
+  console.log(gameGrid);
+  
 }
 
-function gameBoard(player){
-    return {
-
-    }
+function alertTie(){
+  alert('its a tie');
 }
 
-createBoard();
+function Game(){
+  playerTurn = 'X';
+  let winner = false;
+ 
+}
 
+function changePlayer(){
+  if (playerTurn == 'X'){
+    playerTurn = 'O';
+  }
+  else{
+    playerTurn = 'X';
+  }
+}
+
+function computerAI(gameGrid){
+  
+}
